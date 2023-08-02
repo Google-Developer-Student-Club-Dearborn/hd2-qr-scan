20c2415ec5e35ec61716f3121c64c6423b247723
Ali Elzein <alielzein753@gmail.com>
2023-08-03 01:57:45 +0300

    debug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const path = require('path');
 
 const conf = {
-    port: 3001
+    port: 3001 || process.env.PORT
 }
 
 app.use(express.static(path.resolve(__dirname, './client/build')));
