6645960c5eed772f772fa37bcb141e092653bff0
Ali Elzein <alielzein753@gmail.com>
2023-08-03 01:52:19 +0300

    debug

--- api/routine.js
+++ api/routine.js
@@ -7,7 +7,7 @@ const credentials = {
     "type": process.env.SA_TYPE,
     "project_id": process.env.SA_PROJECT_ID,
     "private_key_id": process.env.SA_PRIVATE_KEY_ID,
-    "private_key": process.env.SA_PRIVATE_KEY,
+    "private_key": process.env.SA_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.SA_CLIENT_EMAIL,
     "client_id": process.env.SA_CLIENT_ID,
     "auth_uri": process.env.SA_AUTH_URI,
