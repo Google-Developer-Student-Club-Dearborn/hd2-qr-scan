7043b6a2e1c88c331ca8aeb35f125a349ef2a43e
Ali Elzein <alielzein753@gmail.com>
2023-08-04 02:00:10 +0300

    heroky deploy command #123

--- heroku_deploy_command.sh
+++ heroku_deploy_command.sh
@@ -1,1 +1,6 @@
-npm run build; git add .; git commit -m "heroky deploy command #123"; git push heroku main
+cd client
+npm run build
+cd ..
+git add .
+git commit -m "heroky deploy command #123"
+git push heroku main
