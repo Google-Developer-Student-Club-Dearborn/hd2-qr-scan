5276b4f138509d4f49152c9ae68edfa3f9038fb0
Ali Elzein <alielzein753@gmail.com>
2023-08-04 01:10:36 +0300

    only api this time

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const conf = {
     port: process.env.PORT || 3001
 }
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+// app.use(express.static(path.resolve(__dirname, './client/build')));
 
 //serving the api
 app.use('/api', require('./api'))
 
 // unknown routes go back to the frontend to enable refreshes on the frontend
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+// });
 
 app.listen(conf.port, () => {
     console.log('listening')
