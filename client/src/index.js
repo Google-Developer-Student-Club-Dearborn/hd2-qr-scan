e4da256570e7f97132e12cacc921fc8ef74d298a
Ali Elzein <alielzein753@gmail.com>
2023-08-22 22:07:36 +0300

    branding...

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,15 @@ import style from "./style.module.css"
 import { Card, CircularProgress, Typography } from '@mui/material';
 import Html5QrcodePlugin from './Html5QrcodePlugin';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const App = () => {
 
   const [scannedData, setScannedData] = useState(null);
@@ -85,71 +94,76 @@ const App = () => {
   }
 
   return (
-    <div className={style.appContainer}>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className={style.appContainer}>
 
-      {
-        !resultOpened && <div className={style.scanPage}>
-          <div className={style.cameraContainer}>
-            <Html5QrcodePlugin
-              fps={10}
-              disableFlip={false}
-              qrCodeSuccessCallback={(decodedText, decodedResult) => {
-                console.log(`Code matched = ${decodedText}`, decodedResult);
-                setScannedData(decodedText)
-                openResult(decodedText)
-              }}
-            />
+        {
+          !resultOpened && <div className={style.scanPage}>
+            <div className={style.cameraContainer}>
+              <Html5QrcodePlugin
+                fps={10}
+                disableFlip={false}
+                qrCodeSuccessCallback={(decodedText, decodedResult) => {
+                  console.log(`Code matched = ${decodedText}`, decodedResult);
+                  setScannedData(decodedText)
+                  openResult(decodedText)
+                }}
+              />
+            </div>
+
+            <button style={{ position: "absolute", bottom: 30, right: 30 }} onClick={() => { setScannedData("+12488707620"); openResult("+12488707620") }}>mock scan +12488707620</button>
           </div>
+        }
 
-          <button style={{ position: "absolute", bottom: 30, right: 30 }} onClick={() => { setScannedData("+12488707620"); openResult("+12488707620") }}>mock scan +12488707620</button>
-        </div>
-      }
+        {
+          resultOpened && <div className={style.resultPage}>
+            <Typography sx={{ p: 2 }} variant="h5">Scanned data: {scannedData}</Typography>
+            {
+              loading
+                ?
+                <CircularProgress />
+                : <React.Fragment>
+                  <Card sx={{ p: 2, minWidth: 275 }}>
+                    <Typography>Name: {result?.first_name} {result?.last_name}</Typography>
+                    <Typography>Phone Number: {result?.phone_number}</Typography>
+                    <Typography>Token: {result?.token}</Typography>
+                    <Typography>Attended: {result?.attended ? "Yes" : "No"}</Typography>
+                  </Card>
 
-      {
-        resultOpened && <div className={style.resultPage}>
-          <Typography sx={{ p: 2 }} variant="h5">Scanned data: {scannedData}</Typography>
-          {
-            loading
-              ?
-              <CircularProgress />
-              : <React.Fragment>
-                <Card sx={{ p: 2, minWidth: 275 }}>
-                  <Typography>Name: {result?.first_name} {result?.last_name}</Typography>
-                  <Typography>Phone Number: {result?.phone_number}</Typography>
-                  <Typography>Token: {result?.token}</Typography>
-                  <Typography>Attended: {result?.attended ? "Yes" : "No"}</Typography>
-                </Card>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      setResultOpened(false)
+                    }}>
+                    done
+                  </Button>
 
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    setResultOpened(false)
-                  }}>
-                  done
-                </Button>
+                  {<Button
+                    disabled={result.attended}
+                    variant="contained"
+                    onClick={() => {
+                      attendRegistrant()
+                    }}>
+                    {result.attended ? "attended" : "attend"}
+                  </Button>}
 
-                {!result.attended && <Button
-                  variant="contained"
-                  onClick={() => {
-                    attendRegistrant()
-                  }}>
-                  attend
-                </Button>}
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      raffleAttendee()
+                    }}>
+                    raffle
+                  </Button>
+                </React.Fragment>
+            }
 
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    raffleAttendee()
-                  }}>
-                  raffle
-                </Button>
-              </React.Fragment>
-          }
+          </div>
+        }
 
-        </div>
-      }
+      </div>
+    </ThemeProvider>
 
-    </div>
   );
 };
 
